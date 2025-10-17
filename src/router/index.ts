6ecf6ba07f2356b88266bf100b127c474109f865
v0l0dar <8081@ukr.net>
2025-10-17 14:45:50 +0300

    add dog-viewer page and connect

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import DogViewer from '@/views/DogViewer.vue'
+
+const routes = [{ path: '/', name: 'home', component: DogViewer }]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
---
+++ src/views/DogViewer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>DogViewer</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
